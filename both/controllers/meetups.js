59d6e064c69ffa85a8159eb80b16e23b85fa606f
Gary Glasscock <rathius@icloud.com>
2015-12-21 18:53:53 -0500

    setup meetups files, added routes, edited permisssion

---
+++ both/controllers/meetups.js
@@ -0,0 +1,16 @@
+MeetupsController = AppController.extend({
+  waitOn: function() {
+    return this.subscribe('meetups');
+  },
+  data: {
+    meetups: Meetups.find({})
+  },
+  onAfterAction: function () {
+    Meta.setTitle('Meetups');
+  }
+});
+
+MeetupsController.events({
+  
+  
+});
